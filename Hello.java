8b0e9f7865bc113c8db0266077d702e36e5287be
chenshuaihua <15811282778@163.com>
2020-01-08 15:44:14 +0800

    hello.java fixed

--- Hello.java
+++ Hello.java
@@ -1,9 +1,5 @@
 public class Hello{
-<<<<<<< HEAD
-	public static void main(final String[] args) {
-=======
 	public static void main(String[] args){
->>>>>>> 35f8831c55df83168c5ee87799966dbcf4456f4a
 		for(int i=1;i<=11;i++){
 			System.out.println("Hello World ... " +i);
 		}
